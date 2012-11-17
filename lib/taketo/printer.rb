98cb560ef13c4ac6309f249d2e2f98e8907a87a4
Vladimir <vladimir.yarotsky@gmail.com>
2012-11-17 15:34:32 +0300

    Fix typo

--- lib/taketo/printer.rb
+++ lib/taketo/printer.rb
@@ -21,7 +21,7 @@ module Taketo
       @print_result += ("  " * @indent_level + str.to_s).rstrip.chomp + "\n"
     end
 
-    def put_optional(str = "", value)
+    def put_optional(str, value)
       put "#{str} #{value}" if value
     end
   end
