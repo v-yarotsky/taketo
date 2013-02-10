0adfbcf05a10851ae7cf9a5e6a42802ccd5c5c4c
Vladimir <vladimir.yarotsky@gmail.com>
2013-02-11 00:57:30 +0300

    clean dsl spec up

--- spec/lib/taketo/dsl_spec.rb
+++ spec/lib/taketo/dsl_spec.rb
@@ -33,17 +33,9 @@ describe "DSL" do
       context "under #{parent_scope_name} #{parent_scope_variants.inspect}" do
         it { should enclose_scope(scope_name).under(parent_scope_variants) }
 
-        define_method :within_parent_dsl do |&block|
-          parent_scope_variants.each do |parent_scope_variant|
-            dsl(parent_scope_variant, factory.create(parent_scope_name)) do |c|
-              stub_find_or_create_scope_object(c, scope_name, :bar)
-              block.call(c)
-            end
-          end
-        end
-
         it "creates a #{scope_name} under #{parent_scope_name} and set it as current scope object" do
-          within_parent_dsl do |c|
+          within_parent_dsl(parent_scope_name) do |c|
+            stub_find_or_create_scope_object(c, scope_name, :bar)
             c.send(scope_name, :bar) do
               expect(c.current_scope_object).to eq(factory.send(scope_name))
             end
@@ -51,7 +43,8 @@ describe "DSL" do
         end
 
         it "does not leak #{scope_name} as current scope object" do
-          within_parent_dsl do |c|
+          within_parent_dsl(parent_scope_name) do |c|
+            stub_find_or_create_scope_object(c, scope_name, :bar)
             c.send(scope_name, :bar) do
               expect(c.current_scope_object).to eq(factory.send(scope_name))
             end
@@ -60,7 +53,8 @@ describe "DSL" do
         end
 
         it "adds a #{scope_name} to the #{parent_scope_name}'s #{scope_name}s collection" do
-          within_parent_dsl do |c|
+          within_parent_dsl(parent_scope_name) do |c|
+            stub_find_or_create_scope_object(c, scope_name, :bar)
             c.current_scope_object.should_receive("append_#{scope_name}").
               with(factory.send(scope_name))
             c.send(scope_name, :bar) {}
@@ -68,7 +62,8 @@ describe "DSL" do
         end
 
         it "sets #{scope_name}'s parent to the #{parent_scope_name} scope object" do
-          within_parent_dsl do |c|
+          within_parent_dsl(parent_scope_name) do |c|
+            stub_find_or_create_scope_object(c, scope_name, :bar)
             factory.send(scope_name).should_receive(:parent=).with(c.current_scope_object)
             c.send(scope_name, :bar) {}
           end
@@ -81,12 +76,10 @@ describe "DSL" do
     it_behaves_like "a scoped construct", attribute_name, parent_scope_names, false
 
     Array(parent_scope_names).each do |parent_scope_name|
-      scopes[parent_scope_name].each do |parent_scope_variant|
-        it "calls #{parent_scope_method} on current #{parent_scope_name}" do
-          dsl(parent_scope_variant, factory.create(parent_scope_name, :foo)) do |c|
-            factory.send(parent_scope_name).should_receive(parent_scope_method).with(example_value)
-            c.send(attribute_name, example_value)
-          end
+      it "calls #{parent_scope_method} on current #{parent_scope_name}" do
+        within_parent_dsl(parent_scope_name) do |c|
+          factory.send(parent_scope_name).should_receive(parent_scope_method).with(example_value)
+          c.send(attribute_name, example_value)
         end
       end
     end
@@ -100,12 +93,10 @@ describe "DSL" do
     it_behaves_like "a scoped construct", :default_server_config, [:config, :project, :environment, :group]
 
     it "stores a block" do
-      scopes[:config].each do |parent_scope_variant|
-        dsl(parent_scope_variant, factory.create(:config)) do |c|
-          cfg = proc { any_method_call_here }
-          factory.config.should_receive(:default_server_config=).with(cfg)
-          c.default_server_config(&cfg)
-        end
+      within_parent_dsl(:config) do |c|
+        cfg = proc { any_method_call_here }
+        factory.config.should_receive(:default_server_config=).with(cfg)
+        c.default_server_config(&cfg)
       end
     end
   end
@@ -114,49 +105,41 @@ describe "DSL" do
     it_behaves_like "a scoped construct", :shared_server_config, :config
 
     it "stores a block" do
-      scopes[:config].each do |parent_scope_variant|
-        dsl(parent_scope_variant, factory.create(:config)) do |c|
-          cfg = proc { any_method_call_here }
-          c.shared_server_config(:foo, &cfg)
-          expect(c.shared_server_configs[:foo]).to eq(cfg)
-        end
+      within_parent_dsl(:config) do |c|
+        cfg = proc { any_method_call_here }
+        c.shared_server_config(:foo, &cfg)
+        expect(c.shared_server_configs[:foo]).to eq(cfg)
       end
     end
   end
 
   describe "#include_shared_server_config" do
     it "executes the block on dsl object in server scope for given shared config names" do
-      scopes[:server].each do |parent_scope_variant|
-        dsl(parent_scope_variant, factory.create(:config)) do |c|
-          c.stub(:shared_server_configs => { :foo => proc { call_from_foo }, :bar => proc { call_from_bar } })
-          c.should_receive(:call_from_foo).once
-          c.should_receive(:call_from_bar).once
-          c.include_shared_server_config(:foo, :bar)
-        end
+      within_parent_dsl(:server) do |c|
+        c.stub(:shared_server_configs => { :foo => proc { call_from_foo }, :bar => proc { call_from_bar } })
+        c.should_receive(:call_from_foo).once
+        c.should_receive(:call_from_bar).once
+        c.include_shared_server_config(:foo, :bar)
       end
     end
 
     context "when the argument is hash where shared config names are keys" do
       context "when hash values are arrays" do
         it "includes config corresponding to hash key and passes exploded arguments" do
-          scopes[:server].each do |parent_scope_variant|
-            dsl(parent_scope_variant, factory.create(:config)) do |c|
-              c.stub(:shared_server_configs => { :foo => proc { |a, b| any_method_call_here(a, b) } })
-              c.should_receive(:any_method_call_here).with(321, 322)
-              c.include_shared_server_config(:foo => [321, 322])
-            end
+          within_parent_dsl(:server) do |c|
+            c.stub(:shared_server_configs => { :foo => proc { |a, b| any_method_call_here(a, b) } })
+            c.should_receive(:any_method_call_here).with(321, 322)
+            c.include_shared_server_config(:foo => [321, 322])
           end
         end
       end
 
       context "when hash values are single values" do
         it "includes config corresponding to hash key and passes the argument" do
-          scopes[:server].each do |parent_scope_variant|
-            dsl(parent_scope_variant, factory.create(:config)) do |c|
-              c.stub(:shared_server_configs => { :foo => proc { |qux| any_method_call_here(qux) } })
-              c.should_receive(:any_method_call_here).with(321)
-              c.include_shared_server_config(:foo => 321)
-            end
+          within_parent_dsl(:server) do |c|
+            c.stub(:shared_server_configs => { :foo => proc { |qux| any_method_call_here(qux) } })
+            c.should_receive(:any_method_call_here).with(321)
+            c.include_shared_server_config(:foo => 321)
           end
         end
       end
@@ -164,22 +147,18 @@ describe "DSL" do
 
     context "whne there are symbol arguments (names of shared configs) and a hash" do
       it "includes config corresponding to symbol arguments and hash keys" do
-        scopes[:server].each do |parent_scope_variant|
-          dsl(parent_scope_variant, factory.create(:config)) do |c|
-            c.stub(:shared_server_configs => { :foo => proc { call_from_foo }, :bar => proc { |qux| call_from_bar(qux) } })
-            c.should_receive(:call_from_foo)
-            c.should_receive(:call_from_bar).with(123)
-            c.include_shared_server_config(:foo, :bar => 123)
-          end
+        within_parent_dsl(:server) do |c|
+          c.stub(:shared_server_configs => { :foo => proc { call_from_foo }, :bar => proc { |qux| call_from_bar(qux) } })
+          c.should_receive(:call_from_foo)
+          c.should_receive(:call_from_bar).with(123)
+          c.include_shared_server_config(:foo, :bar => 123)
         end
       end
     end
 
     it "raises ConfigError if non-existent config included" do
-      scopes[:server].each do |parent_scope_variant|
-        dsl(parent_scope_variant, factory.create(:server)) do |c|
-          expect { c.include_shared_server_config(:foo) }.to raise_error(Taketo::DSL::ConfigError, "Shared server config 'foo' is not defined!")
-        end
+      within_parent_dsl(:server) do |c|
+        expect { c.include_shared_server_config(:foo) }.to raise_error(Taketo::DSL::ConfigError, "Shared server config 'foo' is not defined!")
       end
     end
   end
@@ -200,28 +179,20 @@ describe "DSL" do
     it_behaves_like "a scope", :server, [:environment, :config, :project, :group]
 
     it "evaluates default_server_config" do
-      scopes[:environment].each do |parent_scope_variant|
-        dsl(parent_scope_variant, factory.create(:environment, :foo)) do |c|
-          stub_find_or_create_scope_object(c, :server, :bar)
-
-          default_server_config = proc { some_setup }
-          factory.server.stub(:default_server_config => default_server_config)
-
-          c.should_receive(:some_setup)
-
-          c.server(:bar) do
-          end
-        end
+      within_parent_dsl(:environment) do |c|
+        stub_find_or_create_scope_object(c, :server, :bar)
+        default_server_config = proc { some_setup }
+        factory.server.stub(:default_server_config => default_server_config)
+        c.should_receive(:some_setup)
+        c.server(:bar) {}
       end
     end
 
     it "has name optional" do
-      scopes[:environment].each do |parent_scope_variant|
-        dsl(parent_scope_variant, factory.create(:environment, :foo)) do |c|
-          stub_find_or_create_scope_object(c, :server, :default)
-          c.server do
-            expect(c.current_scope_object.name).to eq(:default)
-          end
+      within_parent_dsl(:environment) do |c|
+        stub_find_or_create_scope_object(c, :server, :default)
+        c.server do
+          expect(c.current_scope_object.name).to eq(:default)
         end
       end
     end
@@ -279,16 +250,12 @@ describe "DSL" do
     end
 
     it "corresponds to current scope" do
-      scopes[:project].each do |parent_scope_variant|
-        dsl(parent_scope_variant, factory.create_project(:foo)) do |c|
-          expect(c.current_scope_object).to eq(factory.project)
-        end
+      within_parent_dsl(:project) do |c|
+        expect(c.current_scope_object).to eq(factory.project)
       end
 
-      scopes[:server].each do |parent_scope_variant|
-        dsl(parent_scope_variant, factory.create_server(:foo)) do |c|
-          expect(c.current_scope_object).to eq(factory.server)
-        end
+      within_parent_dsl(:server) do |c|
+        expect(c.current_scope_object).to eq(factory.server)
       end
     end
   end
@@ -322,5 +289,14 @@ describe "DSL" do
   def stub_find_or_create_scope_object(dsl_object, scope, name)
     dsl_object.current_scope_object.stub(:find).with(scope, name).and_yield.and_return(factory.create(scope, name))
   end
+
+  def within_parent_dsl(parent_scope, &block)
+    scopes[parent_scope].each do |parent_scope_variant|
+      dsl(parent_scope_variant, factory.create(parent_scope)) do |c|
+        block.call(c)
+      end
+    end
+  end
+
 end
 
